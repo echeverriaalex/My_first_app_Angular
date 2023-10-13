95f41f21d66a7f39472fe0adc330f035eb664f24
echeverriaalex <aleex.naahuel@outlook.com>
2023-10-12 23:23:01 -0300

    creo componentes y configure router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './Components/product-add/product-add.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: ProductAddComponent},
+  {path: 'list', component: ProductListComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './Components/product-add/product-add.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    NavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
